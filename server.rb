ad1cc7a20d75dcee4e2dc14d282d442725f598de
Milo Wissig <mwissig@gmail.com>
2018-08-14 14:57:59 -0400

    some mobile responsivity

--- server.rb
+++ server.rb
@@ -5,7 +5,8 @@ require 'faker'
 
 $recipes = []
 $phone = Faker::PhoneNumber.phone_number
-$address = Faker::Address.street_address + ", " + Faker::Address.city + " " + Faker::Address.state_abbr + " " + Faker::Address.zip_code
+$city = Faker::Address.city
+$address = Faker::Address.street_address + ", " + $city + " " + Faker::Address.state_abbr + " " + Faker::Address.zip_code
 p $phone
 
 class Recipe
@@ -76,7 +77,7 @@ cookie = Cookie.new("Chocolate Drizzled Lemon Cookies", 4.99, "images/driz.png",
 cookie = Cookie.new("Hazelnut Toffee Truffle Praline Cookies", 4.04, "images/http.png", "Just browsing these decadent cookies will bring back memories.")
 cookie = Cookie.new("Roasted Pineapple Honeycomb Cookies", 7.99, "images/hon.png", "We just thought this sounded good.")
 cookie = Cookie.new("Sage Gingerbread Moose", 8.99, "images/cookie4.png", "Sweet and herbal moose-shaped cookies.")
-cookie = Cookie.new("Iced Sugar Unicode Snowmen", 4.99, "images/cookie4.png", "Everyone's favorite character as a simple iced sugar cookie.")
+cookie = Cookie.new("Iced Sugar Unicode Snowmen", 4.99, "images/man.png", "Everyone's favorite character as a simple iced sugar cookie.")
 cookie = Cookie.new("Apple Cookies", 14.99, "images/cookie.png", "They're much more expensive than Linux Cookies.")
 
 muffin = Muffin.new("Meyer Lemon Poppyseed Muffins", 4.55, "images/lempopmuf.png", "A twist on the classic lemon poppyseed muffin, made with a cross-breed of lemons and tangerines.")
@@ -85,12 +86,12 @@ muffin = Muffin.new("Froot Loops Muffins", 5.75, "images/loop.png", "This muffin
 muffin = Muffin.new("Bran Muffins", 3.45, "images/muffin2.png", "A simple muffin, in stark contrast to our more decadent flavors.")
 muffin = Muffin.new("English Muffins", 5.99, "images/engmuf.png", "As a bakery only allowed to produce cakes, cookies, and muffins, we're trying to sneak a yeast bread in.")
 muffin = Muffin.new("Strawberry, Black Pepper and Basil Muffins", 5.99, "images/straw.png", "Sweet, herbal, peppery muffins with a cream cheese center.")
-muffin = Muffin.new("Ruby Red Grapefruit Sparkling Water Muffins", "SOLD OUT", "images/muffin4.png", "This muffin is only available for a limited time.")
+muffin = Muffin.new("Ruby Red Grapefruit Sparkling Water Muffins", "SOLD OUT", "images/ruby.png", "This muffin is only available for a limited time.")
 muffin = Muffin.new("Fried Pickle Muffins", 5.99, "images/muffin4.png", "Baked with pickle juice and topped with crispy fried pickle pieces.")
 
 cake = Cake.new("Black Forest gateau", 24.99, "images/blafor.png", "A rich chocolate cake flavored with sour cherry brandy.")
 cake = Cake.new("Lavender Angel Food Cake", 26.99, "images/vict.png", "A light, fluffy angel food cake flavored with lavender oil and lemon zest.")
-cake = Cake.new("Erb Garden Cake", 22.99, "images/garcake.png", "Inspired by Frank Sinatra's famous vegetable gardens. Please accept this season's yield of fresh erbs from our courteous servers.")
+cake = Cake.new("Erb Garden Cake", 22.99, "images/garcake.png", "Relax and let our servers bring you this season's yield of fresh erbs.")
 cake = Cake.new("Taiyaki Cake", 15.99, "images/tai.png", "This fish-shaped cake is filled with sweet red bean paste.")
 cake = Cake.new("Spruce Tips, Juniper and Blackened Lemon Cake", 23.99, "images/cake3.png", "This piney lemon and cardamom cake will impress or confuse your guests.")
 cake = Cake.new("Finger Lime Cake", 44.99, "images/green.png", "This cake is topped with the vesicles of red and green Australian finger limes.")
